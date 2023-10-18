fe10159b22ee0958438ad2fe0c7bd822ca2c490b
mouradski <mouradski@live.com>
2023-10-18 15:27:17 +0200

    Refactoring

--- src/main/java/dev/mouradski/sgbnftbot/model/Meta.java
+++ src/main/java/dev/mouradski/sgbnftbot/model/Meta.java
@@ -1,19 +1,18 @@
 package dev.mouradski.sgbnftbot.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
-
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Meta {
     private String name;
     private String description;
     private String image;
     private String dna;
     private Integer edition;
-    private List<Attribute> attributes = null;
 }
